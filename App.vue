c2286058aac84470658cfc68a28a193546e33ce5
andy <33732843@qq.com>
2019-08-19 11:21:13 +0800

    initial vuex experiment

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  mounted() {
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    }
+  },
+  computed: {
+    ...mapState({
+      // 箭头函数可使代码更简练
+      count: state => state.count,
+    })
+  }
+}
+</script>
